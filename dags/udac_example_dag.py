fc47989eafc86ac84caa7c45552fe5b07703444b
tymnicholas <46499723+tymnicholas@users.noreply.github.com>
2020-04-22 22:57:43 +0800

    Add files via upload

---
+++ dags/udac_example_dag.py
@@ -0,0 +1,137 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+    'owner': 'udacity',
+    'start_date': datetime(2019, 1, 12),
+    'end_date' : datetime(2019, 1, 30),
+    'depends_on_past' : False,
+    'retries' : 3,
+    'retry_delay' : timedelta(minutes=5),
+    'email_on_retry': False
+}
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *'
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+# Create and Stage tables to Redshift
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    provide_context=True,
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id='redshift',
+    s3_bucket="udacity-dend",
+    s3_key="log_data",
+    table="staging_events",
+    create_stmt=SqlQueries.create_staging_events_table
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    provide_context=True,
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id='redshift',
+    s3_bucket="udacity-dend",
+    s3_key="song_data",
+    table="staging_songs",
+    create_stmt=SqlQueries.create_staging_songs_table
+)
+
+# Create and Load FACT table
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    provide_context=True,
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id='redshift',
+    create_stmt=SqlQueries.create_songplays_table,
+    sql_query=SqlQueries.songplay_table_insert
+)
+
+# Create and Load DIM tables
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    provide_context=True,
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id='redshift',
+    create_stmt=SqlQueries.create_user_table,
+    sql_query=SqlQueries.user_table_insert
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    provide_context=True,
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id='redshift',
+    create_stmt=SqlQueries.create_song_table,
+    sql_query=SqlQueries.song_table_insert
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    provide_context=True,
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id='redshift',
+    create_stmt=SqlQueries.create_artist_table,
+    sql_query=SqlQueries.artist_table_insert
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    provide_context=True,
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id='redshift',
+    create_stmt=SqlQueries.create_time_table,
+    sql_query=SqlQueries.time_table_insert
+)
+
+
+# Perform data quality check
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    provide_context=True,
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id='redshift',
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+# Define order
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+load_user_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+
+run_quality_checks >> end_operator
---
+++ readme.md
@@ -0,0 +1,35 @@
+# P5 : Data Pipelines with Airflow
+
+## Introduction
+A music streaming company, Sparkify, has decided that it is time to introduce more automation and monitoring to their data warehouse ETL pipelines and come to the conclusion that the best tool to achieve this is Apache Airflow.
+
+They have decided to bring you into the project and expect you to create high grade data pipelines that are dynamic and built from reusable tasks, can be monitored, and allow easy backfills. They have also noted that the data quality plays a big part when analyses are executed on top the data warehouse and want to run tests against their datasets after the ETL steps have been executed to catch any discrepancies in the datasets.
+
+The source data resides in S3 and needs to be processed in Sparkify's data warehouse in Amazon Redshift. The source datasets consist of JSON logs that tell about user activity in the application and JSON metadata about the songs the users listen to.
+
+## Project Description
+This project will introduce you to the core concepts of Apache Airflow. To complete the project, you will need to create your own custom operators to perform tasks such as staging the data, filling the data warehouse, and running checks on the data as the final step.
+
+We have provided you with a project template that takes care of all the imports and provides four empty operators that need to be implemented into functional pieces of a data pipeline. The template also contains a set of tasks that need to be linked to achieve a coherent and sensible data flow within the pipeline.
+
+You'll be provided with a helpers class that contains all the SQL transformations. Thus, you won't need to write the ETL yourselves, but you'll need to execute it with your custom operators.
+
+## DAG
+![](./udacity_p5_airflow_pipeline.PNG)
+
+### Treeview
+![](./udacity_p5_airflow_tree.PNG)
+
+## Datasets 
+For this project, you'll be working with two datasets. 
+
+Log data: s3://udacity-dend/log_data
+Song data: s3://udacity-dend/song_data
+
+## To run
+1. Run `/opt/airflow.start.sh` to start the Airflow server
+2. Click on `Access Airflow` button to access the Airflow UI
+3. Toggle the DAG `On`
+
+## Schema
+![](./P5_Diagram.png)
